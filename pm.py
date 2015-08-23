467e49a4a555f0cb31fd847311b4c70d5a6c74c0
foobiQ <foobiQ@users.noreply.github.com>
2015-08-23 22:20:23 +0200

    command line interface, downloading packages, configuration file

--- pm.py
+++ pm.py
@@ -1,10 +1,29 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2
 
+""" Python2 based package manager. """
+
+import argparse
 import json
-import os.path
+import os
+import stat
 import subprocess
 import tarfile
 import shutil
+from urllib2 import urlopen
+from urlparse import urljoin
+import re
+
+
+emptyConfig = {'packageManagerDir': '~/local/packageManager',
+        'packageRepositoryURL': 'http://',
+        'installationEnvironmentVariables': {'INSTALL_PREFIX': '~/local'},
+        }
+
+_availablePackagesDir = 'availablePackages'
+_installedPackagesDir = 'installedPackages'
+_sourcesDir = 'sources'
+_installScriptsDir = 'installScripts'
+_buildDir = 'build'
 
 
 class PackageError(StandardError): pass
@@ -30,7 +49,7 @@ class Version(object):
         if not isinstance(other, Version):
             return NotImplemented
         # TODO real version comparison
-        return self._versionStr == other._versionStr
+        return self._versionStr != other._versionStr
 
 
 class Package(object):
@@ -45,10 +64,10 @@ class Package(object):
         elif self.packageType == 'meta':
             pass
         elif self.packageType == 'git':
-            # TODO
             pass
+            #raise NotImplementedError
         else:
-            raise ValueError('unknown package type {}'.format(self.packageType))
+            raise ValueError("unknown package type {}".format(self.packageType))
 
     def __str__(self):
         return '{} ({})'.format(self.name, self.version)
@@ -80,39 +99,45 @@ class Package(object):
         with tarfile.open(sourceFile) as tf:
             tf.extractall(path=unpackTo)
 
-    def _runInstallScript(self, buildPath, installScriptsPath):
+    def _runInstallScript(self, buildPath, installScriptsPath, environmentVariables):
         installScript = os.path.abspath(os.path.join(installScriptsPath, self.installScript))
         if not os.path.isfile(installScript):
             raise PackageError("Installation script '{}' of package '{}' doesn't exist".\
                     format(installScript, self))
-        # TODO check if script is executable
         unpackedSource = os.path.join(buildPath, self.name)
-        subprocess.check_call([installScript], cwd=unpackedSource)
+        try:
+            subprocess.check_call([installScript], cwd=unpackedSource, env=environmentVariables)
+        except OSError as e:
+            raise PackageError("Can not call installation script: {}".format(e))
 
-    def install(self, sourcesPath, installScriptsPath, buildPath):
-        print "\n=== installing {} ===".format(self)
+    def install(self, sourcesPath, installScriptsPath, buildPath, environmentVariables):
+        print "\n--- installing {} ---".format(self)
         if self.packageType == 'archive':
             self._unpackSource(sourcesPath, buildPath)
-            self._runInstallScript(buildPath, installScriptsPath)
+            self._runInstallScript(buildPath, installScriptsPath, environmentVariables)
         elif self.packageType == 'meta':
             pass
         elif self.packageType == 'git':
             # TODO
             pass
         else:
-            raise ValueError('unknown package type {}'.format(self.packageType))
+            raise ValueError("unknown package type {}".format(self.packageType))
+
+    def downloadSource(self, sourcesPath):
+        raise NotImplementedError
 
 
 class PackageManager(object):
     def __init__(self, config):
-        self._basePath = config['basePath']
-        self._availablePackagesPath = os.path.join(self._basePath, 'availablePackages')
-        self._installedPackagesPath = os.path.join(self._basePath, 'installedPackages')
-        self._sourcesPath = os.path.join(self._basePath, 'sources')
-        self._installScriptsPath = os.path.join(self._basePath, 'installScripts')
-        self._buildPath = os.path.join(self._basePath, 'build')
+        self._basePath = os.path.expanduser(config['packageManagerDir'])
+        self._availablePackagesPath = os.path.join(self._basePath, _availablePackagesDir)
+        self._installedPackagesPath = os.path.join(self._basePath, _installedPackagesDir)
+        self._sourcesPath = os.path.join(self._basePath, _sourcesDir)
+        self._installScriptsPath = os.path.join(self._basePath, _installScriptsDir)
+        self._buildPath = os.path.join(self._basePath, _buildDir)
 
-        #self._installEnvs = config['installEnvironmentVariables']
+        self._installEnvs = config['installationEnvironmentVariables']
+        self.packageRepoURL = config['packageRepositoryURL']
 
         self._availablePackages = self._readPackageConfigs(self._availablePackagesPath)
         self._installedPackages = self._readPackageConfigs(self._installedPackagesPath)
@@ -142,36 +167,93 @@ class PackageManager(object):
     def getInstalledPackages(self):
         return self._installedPackages
 
-    def installPackage(self, packageName):
-        if packageName in self._installedPackages.keys():
-            print "Package '{0.name}' is already installed in version {0.version}"\
-                    .format(self._installedPackages[packageName])
-        elif packageName in self._availablePackages.keys():
-            package = self._availablePackages[packageName]
-            packageDependencies = [package] + self._getDependencies(package)
-            packagesToInstall = []
-            for p in reversed(packageDependencies):
-                if p not in self._installedPackages.values() and p not in packagesToInstall:
-                    packagesToInstall = [p] + packagesToInstall
+    def installPackages(self, packageNames):
+        packagesToInstall = []
+        for packageName in packageNames:
+            if packageName in self._installedPackages.keys():
+                print "Package '{0.name}' is already installed in version {0.version}"\
+                        .format(self._installedPackages[packageName])
+            elif packageName in self._availablePackages.keys():
+                package = self._availablePackages[packageName]
+                packageDependencies = [package] + self._getDependencies(package)
+                for p in reversed(packageDependencies):
+                    if p not in self._installedPackages.values() and p not in packagesToInstall:
+                        packagesToInstall = [p] + packagesToInstall
+            else:
+                print "Package '{}' is not available".format(packageName)
 
-            print 'The following packages will be installed to satisfy dependencies:'
+        if packagesToInstall:
+            print "The following packages will be installed to satisfy dependencies:"
             for s in packagesToInstall:
-                print '  {}'.format(s)
-            print '\nare you sure? [y/n]'
-            # TODO ask user
+                print "  {}".format(s)
+            print
+            doIt = raw_input("are you sure? [y/n]\n")
+            if doIt.lower() == 'y':
+                print
+                print "=== downloading packages ==="
+                print
+                self.downloadPackages(packagesToInstall)
+                print
+                print "=== installing packages ==="
+                for p in reversed(packagesToInstall):
+                    p.install(self._sourcesPath, self._installScriptsPath, self._buildPath, \
+                            self._installEnvs)
+                    fromPath = os.path.join(self._availablePackagesPath, p.configFile)
+                    toPath = os.path.join(self._installedPackagesPath, p.configFile)
+                    shutil.copy(fromPath, toPath)
 
-            for p in reversed(packagesToInstall):
-                p.install(self._sourcesPath, self._installScriptsPath, self._buildPath)
-                fromPath = os.path.join(self._availablePackagesPath, p.configFile)
-                toPath = os.path.join(self._installedPackagesPath, p.configFile)
-                shutil.copy(fromPath, toPath)
-        else:
-            print "Package '{}' is not available".format(packageName)
+    def _downloadFile(self, url, destination, executable=False):
+        remote = urlopen(url)
+        try:
+            with open(destination, 'w') as local:
+                local.write(remote.read())
+            if executable:
+                st = os.stat(destination)
+                os.chmod(destination, st.st_mode | stat.S_IEXEC)
+        finally:
+            remote.close()
 
-    def selfUpdate(self):
+    def downloadPackages(self, packages):
+        sourcesDirURL = urljoin(self.packageRepoURL, _sourcesDir + '/')
+        installScriptsDirURL = urljoin(self.packageRepoURL, _installScriptsDir + '/')
+        for package in packages:
+            # source file
+            try:
+                sourceFile = package.sourceFile
+                print "downloading sources of {}".format(package)
+                sourceFileURL = urljoin(sourcesDirURL, sourceFile)
+                localSourceFilePath = os.path.join(self._sourcesPath, sourceFile)
+                self._downloadFile(sourceFileURL, localSourceFilePath)
+            except AttributeError:
+                pass
+            # install script
+            try:
+                installScript = package.installScript
+                print "downloading install script of {}".format(package)
+                installScriptURL = urljoin(installScriptsDirURL, installScript)
+                localScriptPath = os.path.join(self._installScriptsPath, installScript)
+                self._downloadFile(installScriptURL, localScriptPath, executable=True)
+            except AttributeError:
+                pass
+
+    def updateAvailablePackages(self):
+        print "Updating available packages repository"
+        availablePackagesURL = urljoin(self.packageRepoURL, _availablePackagesDir + '/')
+        availablePackagesPath = urlopen(availablePackagesURL)
+        availablePackagesHTML = availablePackagesPath.read().decode('utf-8')
+        packageFilePattern = re.compile('".*\.json"')
+
+        packageFileList = map(lambda p: p.strip('"'), packageFilePattern.findall(availablePackagesHTML))
+        for packageFileName in packageFileList:
+            print "downloading {}".format(packageFileName)
+            packageFileURL = urljoin(availablePackagesURL, packageFileName)
+            localFile = os.path.join(self._availablePackagesPath, packageFileName)
+            self._downloadFile(packageFileURL, localFile)
+
+        print "Done"
+
+    def upgradeInstalledPackages(self):
         raise NotImplementedError
-
-    def updateInstalledPackages(self):
         for packageName in self._installedPackages:
             installedPackage = self._installedPackages[packageName]
             availablePackage = self._availablePackages[packageName]
@@ -182,4 +264,33 @@ class PackageManager(object):
                     if packageName in p.dependencies:
                         p.install(self._sourcesPath, self._installScriptsPath, self._buildPath)
 
+def main():
+    supportedCommands = ['install', 'update', 'upgrade']
+    argParser = argparse.ArgumentParser(description=__doc__, \
+            formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    argParser.add_argument('command', choices=supportedCommands)
+    argParser.add_argument('packages', nargs='*', metavar='package', \
+            help="If command==install, the packages to be installed")
+    argParser.add_argument('-c', '--config', default='~/.pmconfig.json', \
+            help="The package manager configuration file.")
+    args = argParser.parse_args()
 
+    # read config
+    # TODO check if config file exists, write template and exit if not
+    with open(os.path.expanduser(args.config)) as f:
+        pmConfig = json.load(f)
+
+    pm = PackageManager(pmConfig)
+
+    if args.command == 'install':
+        if len(args.packages) > 0:
+            pm.installPackages(args.packages)
+        else:
+            argParser.error('No packages to install provided')
+    elif args.command == 'update':
+        pm.updateAvailablePackages()
+    elif args.command == 'upgrade':
+        pm.upgradeInstalledPackages()
+
+if __name__ == '__main__':
+    main()
